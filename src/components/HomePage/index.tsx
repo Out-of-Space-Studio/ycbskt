f8f96ef24d4be32c40669ec2f8f155fb9d29e452
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-01-31 23:03:33 +0800

    Commit

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function Home(): React.ReactElement {
-    return <div id="home"></div>
+    return (
+        <div id="home">
+            <span className='type'>
+                Hi there!
+                <br></br>    
+                Welcome to YCBSKT.
+                <br></br>
+                A Site for YZU CSE Basketball Team.
+            </span>        
+        </div>
+    );
 }
